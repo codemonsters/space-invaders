a6725cae24c051f722a817ab791f2eb768ad08c5
asexto <a.sexto@codemonsters.es>
2018-11-07 21:23:35 +0100

    Añadidos indicadores de prueba en esquinas de pantalla

--- main.lua
+++ main.lua
@@ -1,11 +1,9 @@
-local push = require "modules/push/push" -- https://github.com/Ulydev/push
 log = require "modules/log/log" -- https://github.com/rxi/log.lua
+local push = require "modules/push/push" -- https://github.com/Ulydev/push
 
-GAME_WIDTH, GAME_HEIGHT = 384, 216 -- Usamos una resolución 16:9 divisible por 8 (resolución juego original: 224x256): https://pacoup.com/2011/06/12/list-of-true-169-resolutions/
-COLOR_FONDO = {0.1, 0.1, 0.1}
-COLOR_PRINCIPAL = {0.2, 0.94901960784314, 0.57647058823529}
-
-local window_width, window_height
+GAME_WIDTH, GAME_HEIGHT = 384, 216 -- Usamos una resolución 16:9 divisible por 8 próxima a la del original (224x256): https://pacoup.com/2011/06/12/list-of-true-169-resolutions/
+COLOR_BACKGROUND = {0.1, 0.1, 0.1}
+COLOR_MAIN = {0.2, 0.94901960784314, 0.57647058823529}
 
 function love.load()
     log.level = "trace" -- trace / debug / info / warn / error / fatal
@@ -16,7 +14,7 @@ function love.load()
     love.graphics.setFont(font) -- Copyright kylemaoin 2010: https://fonts2u.com/space-invaders-regular.font
 
     -- Escalado de la ventana del juego (a resolución nativa si estamos en pantalla completa y con una pequeña reducción si estamos en una ventana)
-    window_width, window_height = love.window.getDesktopDimensions()
+    local window_width, window_height = love.window.getDesktopDimensions()
     if not love.window.getFullscreen == true then
         log.debug("Escalando en pantalla completa")
     else
@@ -37,20 +35,20 @@ function love.load()
     log.info("Juego cargado")
 end
 
-love.resize = function(w, h)
-    push:resize(w, h)
-end
-
 function love.update(dt)
     -- TODO: Implementar
 end
 
 function love.draw()
     push:start()
-    love.graphics.setBackgroundColor(COLOR_FONDO)
+    love.graphics.setBackgroundColor(COLOR_BACKGROUND)
     love.graphics.clear(love.graphics.getBackgroundColor())
-    love.graphics.setColor(COLOR_PRINCIPAL)
-    love.graphics.points({0, 0, GAME_WIDTH - 1, GAME_HEIGHT - 1})   -- Píxeles de prueba para localizar esquinas contrarias de la ventana de juego
+    love.graphics.setColor(COLOR_MAIN)
+
+    -- indicadores de prueba en esquinas superior izquierda e inferior derecha
+    love.graphics.line(0, 0, 10, 0); love.graphics.line(0, 0, 0, 10)
+    love.graphics.line(GAME_WIDTH - 1, GAME_HEIGHT - 1, GAME_WIDTH - 11, GAME_HEIGHT - 1); love.graphics.line(GAME_WIDTH - 1, GAME_HEIGHT - 1, GAME_WIDTH - 1, GAME_HEIGHT - 11)
+
     love.graphics.printf("¡HOLA MUNDO!", 0, (GAME_HEIGHT - font:getHeight()) / 2, GAME_WIDTH, "center")
     push:finish()
 end
@@ -61,4 +59,9 @@ end
 
 function love.keyreleased(key, scancode, isrepeat)
     -- TODO: Implementar
+end
+
+love.resize = function(w, h)
+    log.debug("Redimensionando ventana a " .. w .. " x " .. h .. "px")
+    push:resize(w, h)
 end
